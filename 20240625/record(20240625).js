fa9917366afa322e1eeeabccec43711dd65cb0f3
snake0818 <101310056+snake0818@users.noreply.github.com>
2024-11-09 03:45:27 +0800

    Add files via upload

---
+++ 20240625/record(20240625).js
@@ -0,0 +1,16 @@
+const table = document.getElementById('invigilate');
+
+// 頁面截圖
+document.getElementById('btn-screenshot').addEventListener('click', () => {
+  domtoimage.toBlob(table)
+    .then((image) => {
+      // 將截圖保存為圖片
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(image);
+      link.download = 'screenshot.png';
+      link.click();
+    })
+    .catch((error) => {
+      console.error('截圖失敗:', error);
+    });
+});
